e7c58adc242f7d1a0fa995c6fab61566151bb042
GeonHyeok <shon5982@gmail.com>
2023-08-01 16:41:02 +0900

    .

--- spring/src/main/java/xyz/itwill07/aop/JoinPointApp.java
+++ spring/src/main/java/xyz/itwill07/aop/JoinPointApp.java
@@ -5,20 +5,20 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class JoinPointApp {
 	public static void main(String[] args) {
-
 		ApplicationContext context = new ClassPathXmlApplicationContext("07-2_param.xml");
 		JoinPointBean bean = context.getBean("joinPointBean", JoinPointBean.class);
-		System.out.println("=========================================================");
+		System.out.println("==========================================================");
 		bean.add();
-		System.out.println("=========================================================");
+		System.out.println("==========================================================");
 		bean.modify(1000, "홍길동");
-		System.out.println("=========================================================");
+		System.out.println("==========================================================");
 		bean.remove(2000);
-		System.out.println("=========================================================");
+		System.out.println("==========================================================");
 		bean.getName();
-		System.out.println("=========================================================");
-		bean.calc(20, 10);
-		System.out.println("=========================================================");
+		System.out.println("==========================================================");
+		// bean.calc(20, 10);
+		bean.calc(20, 0);
+		System.out.println("==========================================================");
 		((ClassPathXmlApplicationContext) context).close();
 	}
 }
